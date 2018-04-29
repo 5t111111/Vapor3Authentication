1be64fcf0f09263f53b596a879f563eb95181921
Hirofumi Wakasugi <baenej@gmail.com>
2018-04-29 11:42:41 +0900

    Initial configuration

--- Package.swift
+++ Package.swift
@@ -7,11 +7,14 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
 
+        // 🍃 Leaf template engine.
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "FluentSQLite"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
